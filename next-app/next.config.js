8c876a8bb6c7cdea3138f14e5df797d4b13af3e2
c-spider <spdrdev@gmail.com>
2022-08-18 10:17:35 -0700

    App

---
+++ next-app/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://app.cryptowarriorscoin.com/',
+  },
+}
+
+module.exports = nextConfig
